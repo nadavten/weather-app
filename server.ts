7c28cfea312c21ececda22fdc69b794c37c93e0c
Nadav Tenne <nadavten@gmail.com>
2018-07-08 22:46:31 +0300

    .

--- server.ts
+++ server.ts
@@ -1,7 +1,12 @@
 import * as express from 'express';
+import * as path from 'path';
 
 const app = express();
 
 app.use(express.static(__dirname+'/dist'));
+
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
+  });
 
 app.listen(process.env.PORT || 8000);
