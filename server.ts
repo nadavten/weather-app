52ceba59593b5d94e2bd8e71621ae47278a35a42
Nadav Tenne <nadavten@gmail.com>
2018-07-08 23:06:45 +0300

    .

--- server.ts
+++ server.ts
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.static(__dirname + "/dist"));
 
 app.get("/*", function(req, res) {
-  res.sendFile(path.join(__dirname + "/dist/weather-app/index.html"));
+  res.sendFile(path.join(__dirname ,"/dist/weather-app/index.html"));
 });
 
 app.listen(process.env.PORT || 8000);
